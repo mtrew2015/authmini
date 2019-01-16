7a0a48334b2cc7e48ed9530e3c0b806a3ee00c3f
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-01-15 22:40:31 -0500

    changes

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-
-const db = require('./database/dbConfig.js');
-
+const bcrypt = require('bcryptjs');
+const db = require('./database/dbHelpers.js')
 const server = express();
 
 server.use(express.json());
@@ -20,6 +19,37 @@ server.get('/api/users', (req, res) => {
       res.json(users);
     })
     .catch(err => res.send(err));
+});
+
+
+server.post('/api/register', (req, res) => {
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 16); // 16 is how many times it hashes
+  db.insertUser(user)
+    .then(ids => {
+      res.status(201).json({ id: ids[0] })
+    })
+    .catch(err => {
+      res.status(500).send(err)
+    })
+})
+
+
+server.post('/api/login', (req,res) =>{
+  //check that username exists and passwords match
+  const bodyUser = req.body;
+  db.findByUsername(bodyUser)
+  .then(user => {
+      //username valid, password from client == pw from db
+    if(user.length && bcrypt.compareSync(bodyUser.password,users[0].password)){
+      res.json({info: 'success'})
+    } else {
+      res.status(404).json({err: 'invalid password or username'});
+    }
+  })
+  .catch(err => {
+    res.status(500).send(err)
+  })
 });
 
 server.listen(3300, () => console.log('\nrunning on port 3300\n'));
